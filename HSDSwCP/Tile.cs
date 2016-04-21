5450c2c69dbe88dbe7c249055add82d98d6198c3
WizardMarcos <wizardmarcos@gmail.com>
2016-04-21 16:39:16 +0100

    Tile class update. Started map.

--- HSDSwCP/Tile.cs
+++ HSDSwCP/Tile.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Content;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,16 +13,20 @@ namespace HSDSwCP
     class Tile
     {
         private int nFrames, currentFrame, frameTime, currentFrameTime;
+        private Texture2D texture;
+        private Point sprtLoc;
 
         public Rectangle Hitbox { get; private set; }
 
-        public Tile(int posX, int posY, int nAnimFrames)
+        public Tile(ContentManager content, int posX, int posY, int nAnimFrames, string textureName, Point spriteLocation)
         {
             Hitbox = new Rectangle(posX, posY, 16, 16);
             nFrames = nAnimFrames;
             frameTime = 130;
             currentFrameTime = 0;
             currentFrame = 0;
+            texture = content.Load<Texture2D>("Tiles/" + textureName);
+            sprtLoc = spriteLocation;
         }
 
         public void Update(GameTime gametime)
@@ -36,7 +41,11 @@ namespace HSDSwCP
 
         public void Draw(SpriteBatch spriteBatch)
         {
-
+            spriteBatch.Draw(
+                texture: texture,
+                position: Hitbox.Location.ToVector2(),
+                color: Color.White,
+                sourceRectangle: new Rectangle(sprtLoc.X, sprtLoc.Y + (currentFrame * 16), 16, 16));
         }
     }
 }
