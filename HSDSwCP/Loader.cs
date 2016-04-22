bf56a1c92090a40ac82dce084c439136ef5d4f8a
WizardMarcos <wizardmarcos@gmail.com>
2016-04-22 22:51:26 +0100

    Started working on the loader.
    Will load the maps and stuff. Maybe add the saves too here.
    Added some more sprites.

---
+++ HSDSwCP/Loader.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Content;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HSDSwCP
+{
+    class Loader
+    {
+        public Loader() { }
+
+        public Tile[,] LoadTileImage(ContentManager content, string name)
+        {
+            Texture2D map = content.Load<Texture2D>("Maps/" + name);
+            Color[] data1D = new Color[map.Width * map.Height];
+            Color[,] data2D = new Color[map.Width, map.Height];
+            Tile[,] tilemap = new Tile[map.Width, map.Height];
+
+            map.GetData(data1D);
+
+            for (int x = 0; x < map.Width; x++)
+            {
+                for (int y = 0; y < map.Height; y++)
+                {
+                    data2D[x, y] = data1D[x + y * map.Width];
+
+                    if (data2D[x, y].R == 255)
+                        tilemap[x, y] = new Tile(content, x * 16, y * 16, 1, name, new Point(data2D[x, y].G, data2D[x, y].B));
+                }
+            }
+
+            return tilemap;
+        }
+    }
+}
